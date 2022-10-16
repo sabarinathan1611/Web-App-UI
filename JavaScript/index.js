f2e83adbd78b646adbb71a7a84f91ce132005649
Harish <sabarinathan.project@gmail.com>
2022-10-16 19:05:03 +0530

    Date:16/10/22 Update

---
+++ JavaScript/index.js
@@ -0,0 +1,9 @@
+let setting=document.getElementById('setting')
+
+setting.addEventListener("click",()=>{
+// setting.className +=' rotate'
+setting.classList.toggle('rotate')
+// setting.classList.toggle('rerotate')
+console.log("wrk")
+
+})
